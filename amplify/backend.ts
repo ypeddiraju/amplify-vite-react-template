6334af9e556d4c02b9d0169be61278e60f5f2687
Yashwanth Peddiraju <ypeddiraju@delta.local>
2024-06-02 21:56:28 +0530

    simple backend lambda

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,9 +2,10 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
-
+import { myFirstFunction } from './my-first-function/resource';
 defineBackend({
   auth,
   data,
+  myFirstFunction,
  
 });
