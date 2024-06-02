c552ef573a4332981d0dce84b60a8c1359a846ef
Yashwanth Peddiraju <ypeddiraju@delta.local>
2024-06-02 21:49:47 +0530

    NoLambdaBackend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import { myLambdaSes } from './my-lambda-ses/resource';
+
 
 defineBackend({
   auth,
   data,
-  myLambdaSes,
+ 
 });
